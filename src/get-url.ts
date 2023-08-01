5bdce8d0105b6abf3ff5d187abf223c2d9a5addf
ReisDev <matheusdrdj@gmail.com>
2023-08-01 19:57:47 -0300

    removing extension from getURL

--- src/get-url.ts
+++ src/get-url.ts
@@ -1,16 +1,8 @@
 export function getURL(os: string, version: string): string {
-  const ext = (os: string) => {
-    if (os === 'pc-windows-msvc') {
-      return 'zip';
-    } else {
-      return 'tar.gz';
-    }
-  };
-
   const mdbookSitemapGeneratorName: string = `mdbook-sitemap-generator-x86_64-${os}`;
   const baseURL: string =
     'https://github.com/rxdn/mdbook-sitemap-generator/releases/download';
-  const url: string = `${baseURL}/${version}/${mdbookSitemapGeneratorName}.${ext(os)}`;
+  const url: string = `${baseURL}/${version}/${mdbookSitemapGeneratorName}`;
 
   return url;
 }
